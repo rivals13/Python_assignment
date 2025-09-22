ee24c2e7b927b0c21903d0d8c1853b55b902b6b8
Sansar chhetri <sansarchhetri40@gmail.com>
2025-09-22 11:12:01 +0545

    Initial commit: Banking management project

---
+++ Project.py
@@ -0,0 +1,96 @@
+isLoggedin = False
+
+
+def welcome_display():
+    print("Welcome to the banking management system")
+    print("==" * 20)
+    print("select one option")
+    print("--" * 20)
+    print("1 |Admin login 👨🏻‍💻|")
+    print("\n2 |Staff Login 👨🏻‍💼|")
+    print("\n3 |Customer Login 👨🏻‍|")
+    print("\n current satus: 🔴")
+    print("--" * 20)
+
+
+if not isLoggedin:
+    welcome_display()
+
+# so login()  vanne function le chai  k garxa vanda hamilai chai.. kun type ko login garna lako vanera vandinxa!
+
+
+def login(type_var):
+    if type_var == "admin":
+        print()
+        print(f"you are trying to login in as: '{type_var}'")
+        print("--" * 20)
+        admin_login()
+
+    elif type_var == "staff":
+        print()
+        print(f"You are trying to login in as: {type_var}")
+        print("--" * 20)
+
+    elif type_var == "user":
+        print()
+        print(f"You are ttrying to login as: {type_var}")
+        print("--" * 20)
+
+    else:
+        print("invalid type!!")
+        print("--" * 20)
+
+
+def admin_login():
+    print("new function being executed!!")
+    # creds!!  username,email and password hunxa!!
+    # sabai chai correct format ma  hunu paryo!!tries chai  three  times
+    tries = 0
+    while tries < 3:
+        username = input("\nPlease enter your username(LBEF-adXXXXXX):")
+        email = input("Please enter your email:")
+        # things to check in email:
+        # position of @,.com,
+        if email.find("@") and email.find(".com"):
+            print("@ and .com are present!")
+
+        else:
+            pass
+        digits = ""
+        user_tag = username.rstrip("0123456789")
+        for u in username:
+            if u.isdigit():
+                digits += u
+        print(digits)
+        print(user_tag)
+        if digits != "986532" and user_tag != "LBEF-ad":
+            print("the tag or the nunber is not matching!!")
+            print(f"current try: {tries+1}")
+            tries += 1
+
+
+login_type = ""
+while True:
+    choice = int(input("enter the choice(integers only): "))
+    if choice <= 0:
+        print("enter value greater than 0")
+    elif choice > 0 and choice < 4:
+        if choice == 1:
+            login_type = "admin"
+            login(login_type.lower())
+            break
+
+        elif choice == 2:
+            login_type = "staff"
+            login(login_type.lower())
+            break
+
+        elif choice == 3:
+            login_type = "user"
+            login(login_type.lower())
+            break
+        else:
+            print("Invalid choice!!")
+            break
+    else:
+        print("Please select appropriate range")
